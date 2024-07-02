f0e95c0d39764341a453af69ad828fc532d72224
Teodor-Raychev <teodor.raychev@mentormate.com>
2024-07-02 19:19:24 +0300

    updated telescope file filter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
 		opts = {
 			defaults = {
 				layout_strategy = "horizontal",
-        file_ignore_patterns = { "^%.git/", "^node_modules/" },
+        file_ignore_patterns = { "^%.git/", "^node_modules/", "node_modules" },
 				layout_config = {
 					prompt_position = "top",
 				},
